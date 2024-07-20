b95fc45dab486ac1b43de0874fabb21eb6e83591
NatalliaChashchevikLearn <167074241+NatalliaChashchevikLearn@users.noreply.github.com>
2024-07-20 13:23:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#form");
+    const emailInput = document.querySelector("#email");
+
+    form.addEventListener("submit", handleFormSubmit);
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (isEmailValid(emailInput.value)) {
+            clearForm();
+        } else {
+            showError();
+        }
+    }
+
+    function isEmailValid(email) {
+        const emailPattern = /^[^@]+@[^@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
+    }
+
+    function showError() {
+        form.classList.add("error");
+    }
+
+    function clearForm() {
+        form.classList.remove("error");
+        emailInput.value = "";
+    }
+});
 
